bdad7c30b893a98d9e8d58355ae42741c1dcdaa0
Nilton OS <jniltinho@gmail.com>
2016-12-30 12:19:10 -0200

    add new obs version 0.17 desenv

--- ffmpeg_nvenc/install_nvenc.sh
+++ ffmpeg_nvenc/install_nvenc.sh
@@ -45,7 +45,7 @@ chown -R root:root usr/*
 cp -aR usr/* /usr/
 rm -rf usr ffmpeg-3.2.2_sdk7_Ubuntu1604_64Bits.xz
 
-wget https://github.com/jniltinho/oficinadotux/raw/master/ffmpeg_nvenc/obs-studio-portable_20161206.tgz
+wget https://github.com/jniltinho/oficinadotux/raw/master/ffmpeg_nvenc/obs-studio-portable_20161230.tgz
 rm -rf /opt/obs-studio-portable
 tar -xvf obs-studio-portable_*.tgz
 rm -f obs-studio-portable_*.tgz
